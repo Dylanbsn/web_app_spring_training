bdacbbd274e42971ff56284163d90b0badc42466
Dylan.Bussonnais <bussonnais@et.esiea.fr>
2021-12-08 17:35:01 +0100

    Cannot access org.springframework.context.ConfigurableApplicationContext FIXED

--- src/main/java/fr/lernejo/todo/TodoListApp.java
+++ src/main/java/fr/lernejo/todo/TodoListApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoListApp {
     public static void main(String[] args) {
         SpringApplication.run(TodoListApp.class, args);
+
     }
 }
 
